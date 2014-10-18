2ff8e6c1283eb096a7898c1d9dc69a42052b802f
Nick Webster <nick@nick.geek.nz>
2014-10-18 13:22:51 +1300

    Removing references to the Campfyre URL for easier development

--- web/api/submit.php
+++ web/api/submit.php
@@ -3,6 +3,7 @@ require_once "Mail.php";
 
 //Details to login connect to the database
 include("/home1/etherals/mysqlDetails.php");
+include("/home1/etherals/passwords.php");
 $dbname = "etherals_campfyre";
 
 //Connect to the database
@@ -107,14 +108,14 @@ if (!empty($text) && !empty($type) && !empty($ip) && !empty($type)) {
 				'Content-type' => 'text/html'
 			);
 			$smtp = Mail::factory('smtp', array(
-				'host' => 'ssl://box710.bluehost.com',
+				'host' => 'ssl://box710.bluehost.com', //Change these details to your email server for testing, or just comment this section out
 				'port' => '465',
 				'auth' => true,
 				'username' => 'notify@campfyre.org',
-				'password' => 'Er8=[a2xrm*P]9;1*$'
+				'password' => $CAMPFYRE_NOTIFY_EMAIL
 			));
 			$mail = $smtp->send($to, $headers, $body);
-		}
+			}
 
 		if (!empty($email)) {
 			$email = mysqli_real_escape_string($con, $email);
