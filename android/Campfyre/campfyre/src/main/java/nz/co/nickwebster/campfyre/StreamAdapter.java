25ee0af593cce7ef87fcc9d3da3033f358ec045c
Nick Webster <nick@nick.geek.nz>
2014-11-17 20:47:41 +1300

    Image attachments (Imgur only)

--- android/Campfyre/campfyre/src/main/java/nz/co/nickwebster/campfyre/StreamAdapter.java
+++ android/Campfyre/campfyre/src/main/java/nz/co/nickwebster/campfyre/StreamAdapter.java
@@ -12,11 +12,13 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -47,7 +49,7 @@ public class StreamAdapter extends ArrayAdapter<String> {
 	LayoutInflater inflater = context.getLayoutInflater();
 	View rowView = inflater.inflate(R.layout.post_list_row_layout, null, true);
 	TextView txtTitle = (TextView) rowView.findViewById(R.id.postDesign);
-	final ImageView imageView = (ImageView) rowView.findViewById(R.id.imageDesign);
+	final ImageView robofaceView = (ImageView) rowView.findViewById(R.id.imageDesign);
 	TextView commentCounter = (TextView) rowView.findViewById(R.id.commentcountTextView);
 	TextView postTimeText = (TextView) rowView.findViewById(R.id.postTime);
     Button stokeBtn = (Button)rowView.findViewById(R.id.stokeButton);
@@ -56,64 +58,65 @@ public class StreamAdapter extends ArrayAdapter<String> {
 	postTimeText.setText(postTimes.get(position));
 	stokeBtn.setText("STOKE (" + postScores.get(position) + ")");
 
-    //Attachments
-    final Button attachmentBtn = (Button)rowView.findViewById(R.id.attachmentButton);
-    RelativeLayout attachmentLayout = (RelativeLayout)rowView.findViewById(R.id.attachmentLayout);
-    final ImageView attachmentImage = (ImageView)rowView.findViewById(R.id.attachmentImage);
-    if (!attachments.get(position).equals("n/a")) {
-        try {
-            URL attachmentURL = new URL(attachments.get(position));
-            String hostname = attachmentURL.getHost();
-            String[] hostnameArr = hostname.split(Pattern.quote("."));
-            String domainname = hostnameArr[hostnameArr.length-2];
-            if (domainname.equals("imgur")) {
-                String[] imgURL = domainname.split(Pattern.quote("/"));
-                String imageFile = imgURL[imgURL.length-1];
-                try {
-                    String[] fileArr = imageFile.split(Pattern.quote("."));
-                    imageID = fileArr[0];
-                } catch (Exception e) {
-                    imageID = imageFile;
-                }
-
-                //Get and display image from server
-                Runnable getImage = new Runnable() {
-                    @Override
-                    public void run() {
-                        URL url;
-                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                        StrictMode.setThreadPolicy(policy);
-                        try {
-                            url = new URL("http://i.imgur.com/"+imageID+".png");
-                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                            connection.setDoInput(true);
-                            connection.connect();
-                            InputStream input2 = connection.getInputStream();
-                            final Bitmap image = BitmapFactory.decodeStream(input2);
-                            context.runOnUiThread(new Runnable() {
-                                public void run() {
-                                    //Set image
-                                    attachmentImage.setImageBitmap(image);
-                                }
-                            });
-                        } catch (Exception e) {
-                        }
+        //Attachments
+        final Button attachmentBtn = (Button)rowView.findViewById(R.id.attachmentButton);
+        RelativeLayout attachmentLayout = (RelativeLayout)rowView.findViewById(R.id.attachmentLayout);
+        final ImageButton attachmentImage = (ImageButton)rowView.findViewById(R.id.attachmentImage);
+        if (!attachments.get(position).equals("n/a")) {
+            try {
+                URL attachmentURL = new URL(attachments.get(position));
+                String hostname = attachmentURL.getHost();
+                String[] hostnameArr = hostname.split(Pattern.quote("."));
+                String domainname = hostnameArr[hostnameArr.length-2];
+                if (domainname.equals("imgur")) {
+                    attachmentImage.setVisibility(View.VISIBLE);
+                    String[] imgURL = attachments.get(position).split(Pattern.quote("/"));
+                    String imageFile = imgURL[imgURL.length-1];
+                    try {
+                        String[] fileArr = imageFile.split(Pattern.quote("."));
+                        imageID = fileArr[0];
+                    } catch (Exception e) {
+                        imageID = imageFile;
                     }
-                };
 
-                Thread getImageThread = new Thread(getImage);
-                getImageThread.start();
+                    //Get and display image from server
+                    Runnable getImage = new Runnable() {
+                        @Override
+                        public void run() {
+                            URL url;
+                            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                            StrictMode.setThreadPolicy(policy);
+                            try {
+                                url = new URL("http://i.imgur.com/"+imageID+".png");
+                                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                                connection.setDoInput(true);
+                                connection.connect();
+                                InputStream input = connection.getInputStream();
+                                final Bitmap image = BitmapFactory.decodeStream(input);
+                                context.runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        //Set image
+                                        attachmentImage.setImageBitmap(image);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                Log.e("CampfyreApp", e.toString());
+                            }
+                        }
+                    };
 
-            } else {
-                attachmentBtn.setText(attachments.get(position));
-                attachmentLayout.setVisibility(View.VISIBLE);
+                    Thread getImageThread = new Thread(getImage);
+                    getImageThread.start();
+
+                } else {
+                    attachmentBtn.setText(attachments.get(position));
+                    attachmentLayout.setVisibility(View.VISIBLE);
+                }
+            }
+            catch(Exception e){
+                Log.e("CampfyreApp", e.toString());
             }
         }
-        catch(Exception e){
-            attachmentBtn.setText(attachments.get(position));
-            attachmentLayout.setVisibility(View.VISIBLE);
-        }
-    }
 	
 	//Get and display image from server
 	Runnable getIP = new Runnable() {
@@ -127,12 +130,12 @@ public class StreamAdapter extends ArrayAdapter<String> {
 				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 				connection.setDoInput(true);
 				connection.connect();
-				InputStream input2 = connection.getInputStream();
-				final Bitmap profilePicture = BitmapFactory.decodeStream(input2);
+				InputStream input = connection.getInputStream();
+				final Bitmap profilePicture = BitmapFactory.decodeStream(input);
 				context.runOnUiThread(new Runnable() {
                     public void run() {
                         //Set image
-                        imageView.setImageBitmap(profilePicture);
+                        robofaceView.setImageBitmap(profilePicture);
                     }
                 });
 			} catch (Exception e) {
