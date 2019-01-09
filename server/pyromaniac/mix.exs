5780c9a7b4515590be6bc6f8a1b853f63b6829b6
Nick Webster <nick@nick.geek.nz>
2019-01-09 20:50:16 +1300

    More work on the new graphql backend

--- server/pyromaniac/mix.exs
+++ server/pyromaniac/mix.exs
@@ -21,7 +21,7 @@ defmodule Pyromaniac.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:absinthe, "~> 1.4.0"}
+      {:absinthe, "~> 1.4.0"}
     ]
   end
 end
